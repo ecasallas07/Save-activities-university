b9e59d110d9945d1c3357e27b0754c112d4d0eea
Esteban Casallas <ecasallas07@gmail.com>
2024-03-25 23:28:29 -0500

    models migrate and creation templates

--- mysite/notes/admin.py
+++ mysite/notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Activities
 
-# Register your models here.
+
+# Registra los modelos en el interfaz admin de django
+# crear el usuario para admin --> python manage.py createsuperuser
+
+admin.site.register(User)
+admin.site.register(Activities)
+
