9e6e760606ed4bd702f7d428c45984afbca1a508
ecasallas07 <ecasallas07@gmail.com>
2024-04-23 14:28:53 -0500

    front modals for edit in construction, section activities

---
+++ mysite/notes/static/notes/activities.js
@@ -0,0 +1,20 @@
+let modal_edit = document.getElementById('modal_edit')
+let btn_edit = document.getElementById('btn_notes_edit')
+
+let span = document.getElementsByClassName('close')[0]
+
+btn_edit.onclick = function(){
+  modal_edit.style.display = "block"
+}
+
+
+span.onclick = function(){
+  modal_edit.style.display = "none"
+}
+
+window.onclick = function(event){
+
+  if(event.target == modal_edit){
+    modal_edit.style.display = "none"
+  }
+}
